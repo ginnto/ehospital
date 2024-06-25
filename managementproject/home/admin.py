176e03a1cb3d1195c0475a8f7c40920f47df9f1a
ginnto <ginto252@gmail.com>
2024-06-25 14:36:08 +0530

    table added

--- managementproject/home/admin.py
+++ managementproject/home/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Doctors
+from .models import *
 # Register your models here.
 
 
 admin.site.register(Doctors)    #admin.site.register(modelname)
+
+
+
+admin.site.register(Department)
+
+
+
+admin.site.register(Patients)
--- managementproject/home/models.py
+++ managementproject/home/models.py
@@ -2,7 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 class Doctors(models.Model):
     name = models.CharField(max_length=50)
-    department = models.CharField(max_length=50)
     contact = models.CharField(max_length=50)
+    department = models.ManyToManyField(Department)
+    def __str__(self):
+        return self.name
+
+class Patients(models.Model):
+    name = models.CharField(max_length=50)
+    Address = models.TextField()
+    Contact = models.CharField(max_length=50)
+    Symptoms = models.TextField()
+    Doctor = models.ForeignKey(Doctors,on_delete=models.CASCADE)
+    Status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+
