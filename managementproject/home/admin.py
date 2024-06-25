e8475f7523ba8a9a8e8f47935672f38a12ecc7a1
ginnto <ginto252@gmail.com>
2024-06-25 15:23:13 +0530

    table added

--- managementproject/home/admin.py
+++ managementproject/home/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-
-admin.site.register(Doctors)    #admin.site.register(modelname)
-
-
-
-admin.site.register(Department)
+class docadmin(admin.ModelAdmin):
+    list_display = ['name','contact']
+    list_editable = ['contact']
+admin.site.register(Doctors,docadmin)    #admin.site.register(modelname)
 
 
+class deptadmin(admin.ModelAdmin):
+    list_display = ['name']
+admin.site.register(Department,deptadmin)
 
-admin.site.register(Patients)
+
+class ptadmin(admin.ModelAdmin):
+    list_display = ['name','Address','Contact','Symptoms','Doctor','Status']
+    list_editable = ['Address','Contact','Symptoms','Doctor','Status']
+admin.site.register(Patients,ptadmin)
+
