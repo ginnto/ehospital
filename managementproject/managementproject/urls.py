87497359a412d50fc8965f2447ac51b4d71adb71
ginnto <ginto252@gmail.com>
2024-07-03 09:43:09 +0530

    update

--- managementproject/managementproject/urls.py
+++ managementproject/managementproject/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('doctor/', include('doctor.urls')),
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
 
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
